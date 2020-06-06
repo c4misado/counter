e768b9c7623b5f73b1fcaa957eeec4c5e4eb75d3
c4misado <adelmanalec@gmail.com>
2020-06-06 15:30:24 +0300

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+Counter() { function Counter() {
+  // Declares a new state variable, which we will call "count"
+  // Declares a setCount function that allows you to modify the count value
+  const[count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>The counter is at : {count} </p>
+      <button onClick={() => setCount(count + 1)}>>
+        +1
+      </button>
+      <button onClick={() => setCount(count - 1)}>>
+        -1
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
